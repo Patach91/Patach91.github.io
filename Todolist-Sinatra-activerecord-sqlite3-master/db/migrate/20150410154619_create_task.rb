c6bd31649709bf14dd9f336d2c02e3f68ba8d5e8
patach <achouri.ilies@gmail.com>
2015-06-25 10:04:31 +0200

    mon cv

--- Todolist-Sinatra-activerecord-sqlite3-master/db/migrate/20150410154619_create_task.rb
+++
@@ -1,8 +0,0 @@
-class CreateTask < ActiveRecord::Migration
-  def change
-  	create_table :donnees do |t| #create you table within this file before use rake db:migrate
-  		t.string :content
-  		t.timestamps
-  	end
-  end
-end
