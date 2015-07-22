0ffff1a00ea8cf8b62c504d6117ea36ce94e5885
patach <achouri.ilies@gmail.com>
2015-07-22 12:53:31 +0200

    mon cv

--- js/translate.js
+++ js/translate.js
@@ -1,3 +1,3 @@
 function googleTranslateElementInit() {
-  new google.translate.TranslateElement({pageLanguage: 'fr'}, 'google_translate_element');
+  new google.translate.TranslateElement({pageLanguage: 'fr', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, multilanguagePage: true, gaTrack: true, gaId: 'UA-64597750-1'}, 'google_translate_element');
 }
