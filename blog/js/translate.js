1be68292cf50a17b5e659d7d4c14a3425d8b2e29
patach <achouri.ilies@gmail.com>
2015-08-04 20:39:26 +0200

    mon cv

--- blog/js/translate.js
+++
@@ -1,3 +0,0 @@
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement({pageLanguage: 'fr', multilanguagePage: true, gaTrack: true, gaId: 'UA-64597750-1'}, 'google_translate_element');
-}
